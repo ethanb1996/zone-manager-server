b81352e668c32678a062782f04975bfda5fe72df
Your Name <you@example.com>
2024-07-25 23:43:39 +0300

    create api endpoints - zone_routes

---
+++ routes/zone_routes.py
@@ -0,0 +1,16 @@
+from fastapi import APIRouter
+
+zone_router = APIRouter()
+
+@zone_router.post("/zone", response_model=Zone)
+def create_zone(name:str,points: list[list[float]]):
+    return
+
+@zone_router.delete("/zone/{id}", response_model=str)
+def delete_zone(id: int):
+    return
+
+@zone_router.get("/all_zones", response_model=list[Zone])
+def fetch_zones(name:str,points: list[list[float]]):
+    return
+
