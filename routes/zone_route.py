e0d6295e0e7e38dce81429693a98763346c6e0da
Your Name <you@example.com>
2024-07-26 11:16:24 +0300

    link the zone routes to its controller

--- routes/zone_route.py
+++ routes/zone_route.py
@@ -1,18 +1,20 @@
 from fastapi import APIRouter
 
 from models.zone import Zone
+from models.point import Point
+from controllers.zone_controller import Zone_Controller
 
 zone_router = APIRouter()
 
 @zone_router.post("/zone", response_model=Zone)
-def create_zone(name:str,points: list[list[float]]):
-    return
+def create_zone(name:str,points: list[Point]):
+    return Zone_Controller.create_zone(name,points) 
 
 @zone_router.delete("/zone/{id}", response_model=str)
 def delete_zone(id: int):
-    return
+    return Zone_Controller.delete_zone(id)
 
 @zone_router.get("/all_zones", response_model=list[Zone])
-def fetch_zones(name:str,points: list[list[float]]):
-    return
+def fetch_zones():
+    return Zone_Controller.fetch_zones()
 
